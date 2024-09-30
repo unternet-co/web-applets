669b8826a618c3dffacf86766dfd888e85c4afaa
Rupert Manfredi <mail@ruperts.world>
2024-09-30 12:13:43 +0100

    Playground working as expected

--- playground/main.ts
+++ playground/main.ts
@@ -11,6 +11,10 @@ const appletLabel = document.querySelector('label') as HTMLLabelElement;
 const appletStateDisplay = document.querySelector(
   '#applet-state'
 ) as HTMLLabelElement;
+const actionsList = document.querySelector(
+  '#actions-list'
+) as HTMLFieldSetElement;
+const actionForm = document.querySelector('#action-form') as HTMLFormElement;
 
 let applet: Applet;
 const appletHeaders = await applets.getHeaders('/');
@@ -19,11 +23,58 @@ startApplet(appletHeaders[0].url);
 async function startApplet(url: string) {
   const appletUrl = url;
   applet = await applets.load(appletUrl, appletContainer);
+
   appletLabel.innerText = applet.manifest.name;
-  applet.onstateupdated = (state) => {
-    appletStateDisplay.innerText = JSON.stringify(state, null, 2);
-  };
-  applet.dispatchAction('set_name', { name: 'Rupert' });
+  renderActions(applet.manifest.actions);
+  applet.onstateupdated = renderState;
+}
+
+function renderActions(actions) {
+  const actionTemplate = (action) => /*html*/ `
+    <div class="action">
+      <input type="radio" id="${action.id}" name="${action.id}" value="${action.id}" />
+      <label for="${action.id}">
+        <div class="action-name">${action.id}</div>
+        <div class="action-description">${action.description}</div>
+      </label>
+    </div>
+  `;
+
+  actionsList.innerHTML = actions.map(actionTemplate).join('');
+  document.querySelectorAll('.action > input').forEach((radio) => {
+    radio.addEventListener('change', function () {
+      if (this.checked) {
+        renderForm(actions.filter((a) => a.id === this.value)[0]);
+      }
+    });
+  });
+}
+
+function renderForm(action) {
+  const template = (param) => /*html*/ `
+    <div>
+      <label for=${param.id}>${param.id} (${param.type})</label>
+      <p>${param.description}</p>
+      <input type="text" name=${param.id} id=${param.id} placeholder="Parameter value..." />
+    </div>
+  `;
+
+  actionForm.innerHTML = /*html*/ `
+    ${action.params.map(template).join('')}
+    <input type="submit" />
+  `;
+
+  actionForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(actionForm);
+    const formContents = Object.fromEntries(formData.entries());
+    applet.dispatchAction(action.id, formContents as Record<string, string>);
+    actionForm.reset();
+  });
+}
+
+function renderState(state) {
+  appletStateDisplay.innerText = JSON.stringify(state, null, 2);
 }
 
 function handleAppletChange(event: SelectEvent) {
