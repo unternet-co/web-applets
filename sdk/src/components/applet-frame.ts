1223b480e778888d5149bdc3e0470f2f468ff3ee
Rupert <mail@ruperts.world>
2024-12-03 14:47:00 +1100

    Complete applet frame component"

--- sdk/src/components/applet-frame.ts
+++ sdk/src/components/applet-frame.ts
@@ -1,65 +1,74 @@
-import { Applet, AppletDataEvent, applets } from '../index';
+import { Applet, AppletDataEvent, AppletResizeEvent, applets } from '../index';
 
 // TODO: Add resize event handler, and resize DOM element
-
-// resizeContainer(dimensions: { height: number; width: number }) {
-//   this.container.style.height = `${dimensions.height + 2}px`;
-// }
 
 class AppletFrame extends HTMLElement {
   container?: HTMLIFrameElement;
   applet?: Applet;
+  loaded?: boolean;
 
   connectedCallback() {
-    const styles = document.createElement('style');
-    styles.textContent = this.styles;
-    this.appendChild(styles);
+    this.attachShadow({ mode: 'closed' });
 
     this.container = document.createElement('iframe');
-    this.appendChild(this.container);
-  }
+    this.shadowRoot.appendChild(this.container);
 
-  get styles() {
-    return /*css*/ `
-    applet-frame {
-      display: flex;
-      flex-direction: column;
-    }
-
-    applet-frame iframe {
-      border: none;
-    }
-
-    applet-frame:not(.frameless) {
-      border: 1px solid #ddd;
-    }
-
-    applet-frame.frameless {
-      padding: 0 7px;
-    }
-    `;
+    const styles = document.createElement('style');
+    styles.textContent = this.styles;
+    this.shadowRoot.appendChild(styles);
   }
 
   set url(url: string) {
+    // Yeah, I can't remember why I added the timeout...
+    // But you're too scared to remove it aren't you?
     setTimeout(() => this.loadApplet(url), 1);
   }
 
   async loadApplet(url: string) {
     if (!this.container) return;
     this.applet = await applets.load(url, this.container);
-    // if (this.applet.manifest.frameless) this.classList.add('frameless');
+
+    // When data received, bubble the event up
     this.applet.ondata = (dataEvent: AppletDataEvent) => {
       this.dispatchEvent(dataEvent);
     };
+
+    // Resize
+    this.applet.onresize = (resizeEvent: AppletResizeEvent) => {
+      this.resizeContainer(resizeEvent.dimensions);
+    };
+
+    // Emit a load event when loading complete
     this.dispatchEvent(new Event('load'));
+    this.loaded = true;
   }
 
+  // TODO: Handle removing listener
   set data(data: any) {
-    if (this.applet) this.applet.data = data;
-    // In case applet hasn't loaded yet
-    this.addEventListener('load', () => {
+    if (this.applet && this.loaded) {
       this.applet.data = data;
-    });
+    } else {
+      this.addEventListener('load', () => {
+        this.applet.data = data;
+      });
+    }
+  }
+
+  resizeContainer(dimensions: { height: number; width: number }) {
+    this.container.style.height = `${dimensions.height + 2}px`;
+  }
+
+  get styles() {
+    return /*css*/ `
+      applet-frame {
+        display: flex;
+        flex-direction: column;
+      }
+
+      applet-frame iframe {
+        border: none;
+      }
+    `;
   }
 }
 
