39730d1c7e7884bc4eae0c30541508d5ea6727c9
Oli Bridgman <oli.bridgman@gmail.com>
2025-02-06 10:06:34 +1300

    feat(inspector): Create a model file for interacting with ai model
    This is a replica of _some_ of the model setup from operator.
    
    Given an applet we build:
    
    * the system prompt, noting the actions the applet has
    * a json response schema, for selecting the action and params
    
    We then send those with our model and prompt and return the
    result.
    
    **Note**: We are re-creating the openAI provider and the model
    each time, this could be improved, but I wanted to make sure we're
    always in sync with the currently set token.
    
    We might also want to sync with the applet from this file, but
    currently that is the job of the consumer.

---
+++ inspector/web/lib/model.ts
@@ -0,0 +1,96 @@
+import { Applet } from '@web-applets/sdk';
+
+import { createOpenAI } from '@ai-sdk/openai';
+import { store } from './store';
+import { generateObject, jsonSchema } from 'ai';
+
+function getSystemPrompt(applet: Applet) {
+  const prompt = `\
+    In this environment you have access to a set of tools. Here are the functions available in JSONSchema format:
+    ${applet.actions}
+    Choose one or more functions to call to respond to the user's query.
+  `;
+
+  return prompt;
+}
+
+function getResponseSchema(applet: Applet) {
+  const responseSchema = {
+    type: 'object',
+    properties: {
+      tools: {
+        type: 'array',
+        items: {
+          anyOf: applet.actions.map((action) => {
+            const schema: any = {
+              type: 'object',
+              properties: {
+                id: {
+                  type: 'string',
+                  enum: [action.id],
+                },
+              },
+              additionalProperties: false,
+              required: ['id'],
+            };
+
+            if (action.parameters) {
+              schema.properties.arguments = action.parameters;
+
+              // Set some variables that OpenAI requires if they're not present
+              if (!schema.properties.arguments.required) {
+                schema.properties.arguments.required = Object.keys(
+                  schema.properties.arguments.properties
+                );
+              }
+              schema.properties.arguments.additionalProperties = false;
+
+              schema.required.push('arguments');
+            }
+
+            return schema;
+          }),
+        },
+        additionalProperties: false,
+      },
+    },
+    required: ['tools'],
+    additionalProperties: false,
+  };
+
+  return jsonSchema<{ tools: { id: string; arguments?: any }[] }>(
+    responseSchema
+  );
+}
+
+async function getModelResponse(prompt: string, applet: Applet) {
+  /**
+   * @TODO
+   *
+   * We currently re-create this each time, can we store it? We need to
+   * make sure it stays up to date with the token set in localStorage.
+   */
+  const openAIAPIToken = store.get().settings?.openAIAPIToken;
+  const openai = createOpenAI({
+    apiKey: openAIAPIToken,
+    compatibility: 'strict',
+  });
+
+  const model = openai('gpt-4o-mini');
+
+  const systemPrompt = getSystemPrompt(applet);
+  const responseSchema = getResponseSchema(applet);
+
+  const { object } = await generateObject({
+    model,
+    prompt: prompt,
+    system: systemPrompt,
+    schema: responseSchema,
+  });
+
+  return object.tools[0];
+}
+
+export const model = {
+  getModelResponse,
+};
