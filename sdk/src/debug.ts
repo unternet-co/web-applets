78ae809ec848c67e23efcff73fd92ac8c6406c0b
Steven Vandevelde <steven@tokono.ma>
2025-05-23 16:38:21 +0200

    chore: disable logs

--- sdk/src/debug.ts
+++ sdk/src/debug.ts
@@ -1,4 +1,4 @@
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = true // process.env.NODE_ENV === "production";
 
 function log(location: string, ...messages: any[]) {
   if (!isProduction) {
